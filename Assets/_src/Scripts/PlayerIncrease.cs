063907ecf35c7fd6d23cd3f4af327f1a549a666d
AlwaysBusy <stormss_88@mail.ru>
2024-02-24 19:51:58 +0100

    add size controll logic, import DOTWEEN

--- Assets/_src/Scripts/PlayerIncrease.cs
+++ Assets/_src/Scripts/PlayerIncrease.cs
@@ -2,44 +2,36 @@ using System;
 using Cinemachine;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace _src.Scripts
 {
     [RequireComponent(typeof(SphereCollider), typeof(Rigidbody))]
     public class PlayerIncrease : MonoBehaviour
     {
+        // Используется как "уровень" или "сила" игрока, а не как физический размер
         [SerializeField]
-        private float _size = 0.01f;
+        private float _size = 1f;
+
+        // Фактический физический размер игрока в Unity
+        private float _physicalSize = 0.01f;
 
         [SerializeField]
-        private float _minSize;
+        private float _minSize = 0.01f; // Минимальный физический размер
 
         [SerializeField]
-        private float _maxSize;
+        private float _maxSize = 1.0f; // Максимальный физический размер
 
         [SerializeField, Required]
         private CinemachineVirtualCamera _cinemachineVirtualCamera;
 
         [SerializeField]
-        private float _increaseSizeValue = 0.01f;
+        private float _increaseSizeValue = 0.01f; // Значение изменения физического размера
 
-        private void OnTriggerEnter(Collider other)
-        {
-            if (other.CompareTag("Bug") && other.TryGetComponent(out Bug bug))
-            {
-                if (IsBugLowerThenPlayer(bug))
-                {
-                    IncreaseSize();
-                    Destroy(other.gameObject); // Исправлено с Destroy(other) на Destroy(other.gameObject)
-                }
-                else
-                {
-                    DecreaseSize();
-                    Destroy(other.gameObject); // Также исправлено здесь
-                }
-            }
-        }
-
+        public UnityEvent OnIncreaseSize = new();
+        
+        public UnityEvent OnDecreaseSize = new();
+        
         private void OnCollisionEnter(Collision other)
         {
             if (other.gameObject.CompareTag("Bug") && other.transform.TryGetComponent(out Bug bug))
@@ -59,54 +51,39 @@ namespace _src.Scripts
 
         private void IncreaseSize()
         {
-            if (CanChangeSize())
+            if (_physicalSize + _increaseSizeValue <= _maxSize) // Увеличиваем размер, если не превышен максимум
             {
-             
-                float scaleFactor = 1 + _increaseSizeValue; // Пример увеличения на процент
-                transform.localScale *= scaleFactor;
-
-                UpdateCameraOffset(scaleFactor); // Обновляем смещение камеры
-                _size += 1;
+                OnIncreaseSize.Invoke();
+                _size += 1; // Увеличиваем "уровень" игрока
+                _physicalSize += _increaseSizeValue; // Точное увеличение физического размера
+                UpdateScaleAndCameraOffset();
             }
         }
 
         private void DecreaseSize()
         {
-            if (CanChangeSize())
+            if (_physicalSize - _increaseSizeValue >= _minSize) // Уменьшаем размер, если не меньше минимума
             {
-             
-                float scaleFactor = 1 - _increaseSizeValue; // Пример уменьшения на процент
-                transform.localScale *= scaleFactor;
-
-                UpdateCameraOffset(scaleFactor); // Обновляем смещение камеры
-                _size -= 1;
+                OnDecreaseSize.Invoke();
+                _size = Mathf.Max(_size - 1, 1); // Уменьшаем "уровень" игрока, не опускаясь ниже 1
+                _physicalSize -= _increaseSizeValue; // Точное уменьшение физического размера
+                UpdateScaleAndCameraOffset();
             }
         }
 
-        private void UpdateCameraOffset(float scaleFactor)
+        private void UpdateScaleAndCameraOffset()
         {
+            transform.localScale = Vector3.one * _physicalSize; // Применяем физический размер
+    
             var transposer = _cinemachineVirtualCamera.GetCinemachineComponent<CinemachineTransposer>();
             if (transposer != null)
             {
-                // Обновляем только компоненты y и z смещения камеры, оставляя x без изменений
-                Vector3 currentOffset = transposer.m_FollowOffset;
-                transposer.m_FollowOffset = new Vector3(
-                    currentOffset.x, // оставляем x без изменений
-                    currentOffset.y * scaleFactor, // умножаем y на scaleFactor
-                    currentOffset.z * scaleFactor  // умножаем z на scaleFactor
-                );
+                Vector3 baseOffset = new Vector3(transposer.m_FollowOffset.x, transposer.m_FollowOffset.y +_increaseSizeValue, transposer.m_FollowOffset.z - _increaseSizeValue);
+                transposer.m_FollowOffset = baseOffset;
             }
         }
 
 
-        private bool CanChangeSize()
-        {
-      
-            return transform.localScale.x >= _minSize && transform.localScale.y >= _minSize &&
-                   transform.localScale.z >= _minSize && transform.localScale.x <= _maxSize &&
-                   transform.localScale.y <= _maxSize && transform.localScale.z <= _maxSize;
-        }
-
-        private bool IsBugLowerThenPlayer(Bug bug) => bug.Size < _size;
+        private bool IsBugLowerThenPlayer(Bug bug) => bug.Size < _size; // Сравнение уровня игрока и жука
     }
 }
