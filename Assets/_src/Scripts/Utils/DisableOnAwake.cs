d173c6a8b125980b40cd7580bed55aba41e07caa
lekret <lekret399@mail.ru>
2024-04-02 20:44:10 +0300

    Level3 fixes

---
+++ Assets/_src/Scripts/Utils/DisableOnAwake.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace _src.Scripts.Utils
+{
+    public class DisableOnAwake : MonoBehaviour
+    {
+        private void Awake()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
